56b74f90bc0c71ca092ca81f5dc1cc2388cf4ca0
Meekail Zain <zainmeekail@gmail.com>
2023-05-18 19:24:17 -0400

    Removed extraneous hack for partial import

--- distance_metrics/__init__.py
+++ distance_metrics/__init__.py
@@ -1,11 +1,3 @@
-def get_distance_metric(X, metric):
-    # A bit of a hack to allow for partial import during build
-    try:
-        from ._dist_metrics import get_distance_metric
-
-        return get_distance_metric(X, metric)
-    except ModuleNotFoundError:
-        return None
-
+from ._dist_metrics import get_distance_metric
 
 __all__ = ["get_distance_metric"]
