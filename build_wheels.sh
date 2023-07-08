872ab63ed4c4a9424c5c961ef07a2f334c5c5867
Meekail Zain <zainmeekail@gmail.com>
2023-07-08 12:08:34 -0400

    Updated build strategy to use spack instead of conda

--- build_wheels.sh
+++ build_wheels.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-source ~/.profile
-conda install xsimd
-conda list
+git clone -c feature.manyFiles=true https://github.com/spack/spack.git
+spack/bin/spack install xsimd
+spack/bin/spack load xsimd
+
 which python
 python -m pip install cibuildwheel==2.13.1
 
-export CIBW_ENVIRONMENT="SLSDM_SIMD_ARCH='<=sse3'"
 export CIBW_BUILD="cp39*"
 python -m cibuildwheel --output-dir wheelhouse
