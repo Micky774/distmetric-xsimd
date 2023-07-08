b7334a8475e890d6de9ec2a87bbb589c8680f967
Meekail Zain <zainmeekail@gmail.com>
2023-07-08 12:41:31 -0400

    Moved away from bash script

--- build_wheels.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source ~/.profile
-micromamba list
-which python
-python -m pip install cibuildwheel==2.13.1
-
-export CIBW_BUILD="cp39*"
-python -m cibuildwheel --output-dir wheelhouse
