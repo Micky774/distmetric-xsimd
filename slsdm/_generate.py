52ca0d1b45dab0de774ce52187502ebe631852a9
Meekail Zain <zainmeekail@gmail.com>
2023-07-17 16:36:18 -0400

    Update parser for 3.8 compat

--- slsdm/_generate.py
+++ slsdm/_generate.py
@@ -61,11 +61,11 @@ def _parse_spec(spec, arch):
         for a in _x86[target_arch_idx:]:
             # Ensure unsupported/mutually-exclusive FMA features are not enabled
             if "fma" not in a or a[3] == fma_version:
-                out |= {a: None}
+                out[a] = None
     if "<=" in spec or not spec:
-        out |= {_x86[target_arch_idx]: None}
+        out[_x86[target_arch_idx]] = None
     if "!" in spec:
-        out -= {_x86[target_arch_idx]: None}
+        out.pop(_x86[target_arch_idx], None)
     return out
 
 
